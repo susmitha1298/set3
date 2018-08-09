e75b3150d032dcafa789b8ae5583f47ed000367f
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-08-09 11:07:43 +0530

    Create stringnumeric.java

---
+++ stringnumeric.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String string = "12345.15";
+        boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(string);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println(string + " is a number");
+        else
+            System.out.println(string + " is not a number");
+    }
+}

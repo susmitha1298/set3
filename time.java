76c95924bb74b229501a56179fde109e01c10301
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-08-09 11:55:53 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner in = new Scanner(System.in);
+        int minutes = in.nextInt(); 
+        int p1 = minutes % 60;
+        int p2 = minutes / 60;
+        System.out.print( p2 + " " + p1);	// your code goes here
+	}
+}
